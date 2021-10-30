e1908636a707bd777d32ccbe1418a460fc776630
hectorsantiago5 <hector.santiagomarti@gmail.com>
2021-10-29 23:31:52 -0400

    * Started connecting the bot with the training data and beginnings of connecting bot with HTML files.

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import os
 from urllib.request import urlretrieve as retrieve
 from werkzeug.utils import secure_filename
 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+from config import flow
+
 from flask import Flask, render_template, request, redirect, flash, url_for
+
+bot = ChatBot("SuitBot")
+trainer = ListTrainer(bot)
+trainer.train(flow)
+
 
 app = Flask(__name__)
 
@@ -11,6 +21,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/get")
+def get_bot_response():
+    user_response = request.args.get('msg')
+    return str(bot.get_response(user_response))
 
 
 app.secret_key = 'some_secret_key'
